f6eb083eed9f9b2a5bee05c5c8cb34c4e83a02ff
ndxbxrme <lewis_the_cat@hotmail.com>
2015-08-29 09:37:40 +0100

    meteor

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ndxbxrme:angular-multi-check',
-  version: '0.3.2',
+  version: '0.4.0',
   summary: 'an angular multi-checkbox directive',
   git: 'https://github.com/ndxbxrme/angular-multi-check',
   documentation: 'README.md'
